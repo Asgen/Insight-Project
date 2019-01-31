726c2257e45694a12beab8c760bbd848f1583988
Asgen <genuus1@gmail.com>
2019-01-31 16:55:03 +0300

    Добавляет скрипт смены контента без перезагрузки

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,38 +1,54 @@
 'use strict';
-var openButton = document.querySelector('.page-header__menu-button');
-var menu = document.querySelector('.main-nav');
-var closeButton = menu.querySelector('.main-nav__close');
-var links = document.querySelector('.main-nav__list');
+(function () {
+  var openButton = document.querySelector('.page-header__menu-button');
+  var closeButton = document.querySelector('.main-nav__close');
 
-var link1 = document.querySelector('.main-nav__item:first-of-type');
-var link2 = document.querySelector('.main-nav__item:nth-of-type(2)');
-var link3 = document.querySelector('.main-nav__item:nth-of-type(3)');
-var link4 = document.querySelector('.main-nav__item:nth-of-type(4)');
+  var closeMenu = function () {
+    var menu = document.querySelector('.main-nav');
+    var link1 = document.querySelector('.main-nav__item:first-of-type');
+    var link2 = document.querySelector('.main-nav__item:nth-of-type(2)');
+    var link3 = document.querySelector('.main-nav__item:nth-of-type(3)');
+    var link4 = document.querySelector('.main-nav__item:nth-of-type(4)');
+    var link5 = document.querySelector('.main-nav__item:nth-of-type(5)');
 
-openButton.addEventListener('click', function () {
-  link1.style.marginTop = '200px';
-  link2.style.marginTop = '150px';
-  link3.style.marginTop = '100px';
-  link4.style.marginTop = '50px';
+    menu.style.opacity = '0';
+    menu.style.zIndex = '0';
+    menu.style.visibility = 'hidden';
 
-  menu.style.zIndex = '1';
-  menu.style.opacity = '1';
-  menu.style.visibility = 'visible';
+    link1.style.transform = 'translateY(100px)';
+    link2.style.transform = 'translateY(150px)';
+    link3.style.transform = 'translateY(200px)';
+    link4.style.transform = 'translateY(250px)';
+    link5.style.transform = 'translateY(250px)';
+  };
 
-  link1.style.marginTop = '0';
-  link2.style.marginTop = '0';
-  link3.style.marginTop = '0';
-  link4.style.marginTop = '0';
+  var openMenu = function () {
+    var menu = document.querySelector('.main-nav');
+    var link1 = document.querySelector('.main-nav__item:first-of-type');
+    var link2 = document.querySelector('.main-nav__item:nth-of-type(2)');
+    var link3 = document.querySelector('.main-nav__item:nth-of-type(3)');
+    var link4 = document.querySelector('.main-nav__item:nth-of-type(4)');
+    var link5 = document.querySelector('.main-nav__item:nth-of-type(5)');
 
-});
+    link1.style.transform = 'translateY(100px)';
+    link2.style.transform = 'translateY(150px)';
+    link3.style.transform = 'translateY(200px)';
+    link4.style.transform = 'translateY(250px)';
+    link5.style.transform = 'translateY(250px)';
 
-closeButton.addEventListener('click', function () {
-  menu.style.opacity = '0';
-  menu.style.zIndex = '0';
-  menu.style.visibility = 'hidden';
+    menu.style.zIndex = '1';
+    menu.style.opacity = '1';
+    menu.style.visibility = 'visible';
 
-  link1.style.marginTop = '200px';
-  link2.style.marginTop = '150px';
-  link3.style.marginTop = '100px';
-  link4.style.marginTop = '50px';
-});
+    link1.style.transform = 'translateY(0)';
+    link2.style.transform = 'translateY(0)';
+    link3.style.transform = 'translateY(0)';
+    link4.style.transform = 'translateY(0)';
+    link5.style.transform = 'translateY(0)';
+  };
+
+  openButton.addEventListener('click', openMenu);
+  closeButton.addEventListener('click', closeMenu);
+
+  window.closeMenu = closeMenu;
+})();
---
+++ source/js/reload.js
@@ -0,0 +1,67 @@
+var databox;
+
+var init = function () {
+  databox = document.querySelector('.content');
+  var link = document.querySelectorAll('.main-nav__link');
+  var logoLink = document.querySelector('.main-nav__logo');
+  if(link) {
+    link.forEach(function (elem) {
+      elem.addEventListener('click', read);
+    });
+  }
+  logoLink.addEventListener('click', read);
+};
+
+var read = function (e) {
+  e.preventDefault();
+
+  var path;
+  var url;
+
+  var activeLink = document.querySelector('.main-nav__link--active');
+  if(activeLink) {
+    activeLink.classList.remove('main-nav__link--active');
+  }
+
+  if (e.target.href) {
+    path = e.target.href;
+    var attribute = 'a[href="' + path.slice(22) + '"]';
+    var currentLink = document.querySelector(attribute);
+    currentLink.classList.add('main-nav__link--active');
+    url = path.slice(22, -4) + 'txt'; // 22 символа - это корневой путь, -4 расширение html
+  }
+  else {
+    path = e.target.parentElement.href;
+    url = 'index.txt';
+  }
+
+  console.log(path);
+  //if (e.target.href === window.location.href) {
+  //  return; http://localhost:3000/about.html
+  //}
+
+
+  var request = new XMLHttpRequest();
+  request.addEventListener('load', show);
+  request.open('GET', url, true);
+  request.send(null)
+};
+
+var show = function (e) {
+  var data = e.target;
+
+  //console.log(e.target.responseURL.slice(22, -3));
+
+  if(data.status == 200){
+    databox.innerHTML = data.responseText;
+    var state = { 'page_id': 1, 'user_id': 5 };
+    var title = 'Hello World';
+    var url = e.target.responseURL.slice(22, -3) + 'html';
+
+    history.pushState(state, title, url);
+    window.closeMenu();
+    document.title = e.target.responseURL.slice(22, -4) + ' Insight';
+  }
+};
+
+addEventListener('load', init);
