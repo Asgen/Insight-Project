7d27f6fa74f794db83946aa024b18246f09ffefa
Asgen <genuus1@gmail.com>
2019-02-10 20:47:59 +0300

    Межстраничная анимация

--- source/js/mutations.js
+++ source/js/mutations.js
@@ -2,10 +2,31 @@
 (function () {
   var pageTitle = document.querySelector('title');
   var logo = document.querySelector('.page-header__logo');
+  //var loader = document.querySelector('.loader');
+  //var columns = loader.querySelectorAll('.loader__column span');
 
   new MutationObserver(function(mutations) {
-    console.log('matations detected');
     var func = window.funcs;
+
+    /*// Лоадер
+    loader.style.display = 'block';
+    columns.forEach(function(el, i) {
+      el.style.width = '0';
+      el.style.transition = 'width ' + (0.2 + (i * 0.1)) + 's ease-in-out';
+    });
+
+
+    setTimeout(function() {
+      columns.forEach(function(el) {
+        el.style.width = '100%';
+      });
+    }, 10);
+
+
+    setTimeout(function() {
+      loader.style.display = 'none';
+    }, 1000);*/
+
     // Если страница отзывов
     if(mutations[0].target.textContent === window.data.PageTitle.REVIEWS) {
       func.changeSlides();
--- source/js/reload.js
+++ source/js/reload.js
@@ -1,4 +1,5 @@
 'use strict';
+// Смена страницы (контента) без перезагрузки вкладки браузера
 (function () {
   var databox;
   var pageTitle = document.querySelector('title');
@@ -8,7 +9,9 @@
     databox = document.querySelector('.content');
     var link = document.querySelectorAll('.main-nav__link');
     var logoLink = document.querySelector('.main-nav__logo');
+    var headerLogo = document.querySelector('.page-header__logo');
 
+    // Если выполняется нажатие в браузере стрелок "назад" "вперед"
     if (e.type === 'popstate') {
       read(e);
     } else {
@@ -16,20 +19,22 @@
           elem.addEventListener('click', read);
       });
       logoLink.addEventListener('click', read);
+      headerLogo.addEventListener('click', read);
     }
   };
 
   var read = function (e) {
     var path;
     var url;
+    var request = new XMLHttpRequest();
     var activeLink = document.querySelector('.main-nav__link--active');
     if(activeLink) {
       activeLink.classList.remove('main-nav__link--active');
     }
 
     if (e.type !== 'popstate') {
-     e.preventDefault();
-     if (e.target.href) {
+      e.preventDefault();
+      if (e.target.href) {
       path = e.target.href;
       var attribute = 'a[href="' + path.slice(DIR) + '"]';
       var currentLink = document.querySelector(attribute);
@@ -42,6 +47,7 @@
         path = e.target.parentElement.href;
         url = 'index.txt';
       }
+      request.addEventListener('load', function (e) {show(e, 'click')});
     } else {
       path = e.target.location.href;
       var attribute = 'a[href="' + path.slice(DIR) + '"]';
@@ -50,21 +56,22 @@
         currentLink.classList.add('main-nav__link--active');
       }
       url = path.slice(DIR, -4) + 'txt'; // 4 символа - отсекаем расширение html
+      request.addEventListener('load', function (e) {show(e, 'browser')});
     }
 
-    var request = new XMLHttpRequest();
-    request.addEventListener('load', show);
     request.open('GET', url, true);
     request.send(null)
   };
 
-  var show = function (e) {
+  var show = function (e, sourse) {
     var data = e.target;
     var path = e.target.responseURL.slice(DIR, -4);
 
     if(data.status == window.data.Code.SUCCESS){
+      window.funcs.playTransition();
+      // Отрисовка контента
       databox.innerHTML = data.responseText;
-      if (e.type === 'load') {
+      if (sourse === 'click') {
         var state = { 'page_id': 1, 'user_id': 5 };
         var title = 'Insight';
         var url = path + '.html';
@@ -80,5 +87,5 @@
   };
 
   window.addEventListener('load', init);
-  //window.addEventListener('popstate', init);
+  window.addEventListener('popstate', init);
 })();
