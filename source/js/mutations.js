79c1233ecc81a10c7a56e19dbbd95a729d30b46a
Asgen <genuus1@gmail.com>
2019-02-19 15:43:29 +0300

    change instagram link

--- source/js/mutations.js
+++ source/js/mutations.js
@@ -8,25 +8,6 @@
   new MutationObserver(function(mutations) {
     var func = window.funcs;
 
-    /*// Лоадер
-    loader.style.display = 'block';
-    columns.forEach(function(el, i) {
-      el.style.width = '0';
-      el.style.transition = 'width ' + (0.2 + (i * 0.1)) + 's ease-in-out';
-    });
-
-
-    setTimeout(function() {
-      columns.forEach(function(el) {
-        el.style.width = '100%';
-      });
-    }, 10);
-
-
-    setTimeout(function() {
-      loader.style.display = 'none';
-    }, 1000);*/
-
     // Если страница отзывов
     if(mutations[0].target.textContent === window.data.PageTitle.REVIEWS) {
       func.changeSlides();
